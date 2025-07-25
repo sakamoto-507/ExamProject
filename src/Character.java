25a834d8ab6bbf1b8fba9fe4a337bc7e39fd2c97
SakamotoChisato <241127@std.hi-joho.ac.jp>
2025-07-25 11:06:44 +0900

    a

--- src/Character.java
+++ src/Character.java
@@ -2,9 +2,9 @@ public abstract class Character implements Creature {
     private String name;
     private int hp;
 
-    public Character(String s, int hp) {
-        setName(s);
-        setHp(hp);
+    public Character(String n, int hp) {
+        this.name = n;
+        this.hp = hp;
         if (hp < 0) {
             throw new IllegalArgumentException("初期設定に誤りがあるため、キャラクターを作成できませんでした");
         }
